fea1eface16b743b9e3723bfc2d2db3607020cba
Mackaber <mkbravo555@gmail.com>
2012-01-28 23:54:18 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quelees::Application.config.secret_token = 'cfc64f54a9240603303512459d450e78e5b55f9d3a564910ff297a24c245db1b99899932ed70ecb1c63459a3963087aa1d329164d4a40a37293426d6a7d7492e'
